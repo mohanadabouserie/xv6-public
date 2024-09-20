1732d65fcb4134a4cc4e24805fadcdb63575dc80
Mohamed Abdelmagid <68608858+MohamedEbrahem1@users.noreply.github.com>
2024-09-20 05:00:48 +0000

    Added square root functionality

--- printstats.c
+++ printstats.c
@@ -1,7 +1,23 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-
+#define error 0.01
+float sqrt(int x){
+  if(x==0)return 0;
+  if(x==1) return 1;
+  float result = (float)x;
+  float left = 0;
+  float right = x;
+  while(right-left>error){
+    result = (right+left)/2;
+    if(result*result == x) return result;
+    if(result*result > x) right = right-(right-left)/2;
+    if(result*result < x) left = left+(right-left)/2; 
+    printf(1,"left value is: %d\nRight value is: %d\n",(int)left,(int)right);
+  }
+  printf(1,"The final result is: %d\n: ", (int)result);
+  return result;
+}
 // A program called “printstats” that takes a list of numbers and prints their average, standard deviation, median, min and max.
 int
 main(int argc, char *argv[])
@@ -21,7 +37,8 @@ if(max < atoi(argv[i])){
   }
     
   int mean = sum/(argc-1);
-  printf(1, "%s%d\n%s%d\n%s%d\n", "The mean is: ",mean, "The maximum is: ", max,"The minimum is: ", min);   
+  int test = (int) sqrt(mean);
+  printf(1, "%s%d\n%s%d\n%s%d\n", "The mean is: ",test, "The maximum is: ", max,"The minimum is: ", min);   
   exit();
 }
 
