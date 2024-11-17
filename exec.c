a79db510822e17e2010ddac2a162e8e944bb6a50
Fatemah <master@BigBrain>
2024-11-17 23:58:20 +0200

    changes to exec.c

--- exec.c
+++ exec.c
@@ -18,7 +18,6 @@ exec(char *path, char **argv)
   struct proghdr ph;
   pde_t *pgdir, *oldpgdir;
   struct proc *curproc = myproc();
-  curproc->priority = 3;
 
   begin_op();
 
@@ -100,6 +99,7 @@ exec(char *path, char **argv)
   curproc->sz = sz;
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
+  curproc->priority = 3;
   switchuvm(curproc);
   freevm(oldpgdir);
   return 0;
